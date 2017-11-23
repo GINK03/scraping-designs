80c6627e77dfb26a61e5612d812b3803976aec6f
gimpei <gim.kobayashi@gmail.com>
2017-11-24 01:03:55 +0900

    a

--- parse.py
+++ parse.py
@@ -11,11 +11,25 @@ import concurrent.futures
 def _map(name):
 
   save_name = 'texts/' + name.split('/').pop()
+  link_name = 'links/' + name.split('/').pop()
   if os.path.exists(save_name) is True:
     return 
   html = open(name).read() 
   soup = bs4.BeautifulSoup( html , 'html.parser' )
- 
+
+  links = []
+  for a in soup.find_all('a', href=True):
+    link = a['href']
+    try:
+      if link[0] == '/':
+        link = 'http://wwww.itmedia.co.jp' + link
+    except:
+      continue
+    links.append( link )
+  l = open(link_name, 'w')
+  for link in links:
+    l.write( link + '\n' )
+    
   try:
     contents  = " ".join( [p.text for p in soup.find('div', {'id': 'cmsBody'}).find_all('p')] )
   except Exception:
