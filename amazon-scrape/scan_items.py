8edef99e0b74caa489e0c35669165261bb0d649c
nardtree <gim.kobayashi@gmail.com>
2018-01-20 20:55:35 +0900

    保存時に圧縮をサポート

--- amazon-scrape/scan_items.py
+++ amazon-scrape/scan_items.py
@@ -12,8 +12,8 @@ import re
 
 import hashlib
 def _map(arg):
-  name = arg
-  html = gzip.decompress(open(name, 'rb').read())
+  index, name = arg
+  html = gzip.decompress(open(name, 'rb').read()).decode()
   soup = bs4.BeautifulSoup(html, 'lxml')
 
   for script in soup(["script", "style"]):
@@ -22,13 +22,13 @@ def _map(arg):
   center = soup.find('div', {'id':'centerCol'})
   if center is None:
     return
-  print( name )
+  print( index, name )
   text = re.sub(r'\s{1,}', ' ', center.text.strip())
 
   sha = hashlib.sha256( bytes(text, 'utf8') ).hexdigest()
 
   open('items/{}'.format(sha), 'w').write( text )
 
-names = [name for name in glob.glob('htmls/*')]
+args = [(index,name) for index,name in enumerate(glob.glob('htmls/*'))]
 with concurrent.futures.ProcessPoolExecutor(max_workers=64) as exe:
-  exe.map( _map, names)
+  exe.map( _map, args)
