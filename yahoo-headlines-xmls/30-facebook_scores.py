aa159a7473f50c1cd165143b0d4576a11a5b242c
nardtree <gim.kobayashi@gmail.com>
2018-09-29 16:46:38 +0900

    update

--- yahoo-headlines-xmls/30-facebook_scores.py
+++ yahoo-headlines-xmls/30-facebook_scores.py
@@ -11,42 +11,62 @@ accs = [ line.strip() for line in open('tokens') ]
 proxys = [ {'http':f'http://{es[0]}:{es[1]}'} for es in [ line.strip().split() for line in open('proxys') ] ]
 print(proxys)
 if '--scan' in sys.argv:
-  # make loop
-
-  #while True:
-  for index, path in enumerate(Path('./darturl_clean').glob('*')):
-    key = index%len(accs)
-    acc = accs[key]
-
-    proxy = proxys[ index%len(proxys) ]
-    print(acc)
-    url   = path.open().read()
-    url_hash = sha256(bytes(url,'utf8')).hexdigest() 
-    if os.path.exists(f'facebook_score/{url_hash}'):
-      continue
+  import random
+  import datetime
+  def graph_access(url, acc, obj=None):
     query = f'https://graph.facebook.com/?id={url}&fields=og_object{{engagement}},engagement&access_token={acc}'
     r     = requests.get(query, proxies=proxy)
-    obj   = json.loads(r.text)
-    obj['url'] = url
-    tdatetime = datetime.now()
+    fb_obj   = json.loads(r.text)
+    tdatetime = datetime.datetime.now()
     eval_time = tdatetime.strftime('%Y-%m-%d %H:%M:%S')
-    obj['eval_time'] = eval_time
-    if obj.get('error'):
-      print('error', obj['error']['message'])
-      print(obj)
-      if 'Cannot specify an empty identifier' == obj['error']['message']:
+    
+    obj = {} if obj is None else obj
+    obj['url']       = url
+    obj[eval_time] = fb_obj
+    if fb_obj.get('error'):
+      print('error', fb_obj['error']['message'])
+      print(fb_obj)
+      if 'Cannot specify an empty identifier' == fb_obj['error']['message']:
         path.unlink()
         time.sleep(5.0)
-        continue
-      time.sleep(35.0)
-      continue
+        return
+      time.sleep(30.0)
+      return
     datum = json.dumps(obj, indent=2, ensure_ascii=False)
     print(datum) 
-    open(f'facebook_score/{url_hash}', 'w').write( datum )
-    time.sleep(1.5)
+    open(f'facebook_score_v2/{url_hash}', 'w').write( datum )
+    
+  # make loop
+  while True:
+    for index, path in enumerate(Path('darturl_clean').glob('*')):
+      key = index%len(accs)
+      acc = accs[random.randint(0, len(accs)-1)]
+
+      proxy = proxys[ index%len(proxys) ]
+      print(acc)
+      url   = path.open().read()
+      url_hash = sha256(bytes(url,'utf8')).hexdigest() 
+      if os.path.exists(f'facebook_score_v2/{url_hash}'):
+        obj = json.load(Path(f'facebook_score_v2/{url_hash}').open())
+        eval_times = [datetime.datetime.strptime(eval_time, '%Y-%m-%d %H:%M:%S') for eval_time in obj.keys() if eval_time != 'url']
+        eval_times = sorted(eval_times) 
+        now_datetime = datetime.datetime.now()
+
+        delta_time = now_datetime - eval_times[-1]
+        print(eval_times)
+        # 三時間たびにスキャンする
+        if delta_time.seconds > 3600 * 3:
+          graph_access(url, acc, obj)
+        else:
+          continue
+      else:
+        graph_access(url, acc, None)
+        ...
+      
+      time.sleep(2)
 
 if '--clean' in sys.argv:
-  for path in Path('./facebook_score').glob('*'):
+  for path in Path('./facebook_score_v2').glob('*'):
     obj = json.load(path.open())
     if obj.get('error') is not None:
       path.unlink()
